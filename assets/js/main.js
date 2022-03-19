a7fc8a661a123927eb4818d6aa11631971f5f944
zfayz <95642949+zfayz@users.noreply.github.com>
2022-03-19 21:06:21 +0600

    newArrival section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,37 @@
 let btn = document.querySelector(".btn");
 let container = document.querySelector(".cont");
-let close = document.querySelector(".close");
-btn.addEventListener('click' , function(){
-    container.classList.toggle('show');
+let clo = document.querySelector(".close");
+
+let btn1 = document.querySelector(".btn1");
+let container1 = document.querySelector(".cont1");
+let clo1 = document.querySelector(".close1");
+
+let btn2 = document.querySelector(".btn2");
+let container2 = document.querySelector(".cont2");
+let clo2 = document.querySelector(".close2");
+
+
+function modalPopUp( x , y , z ) {
+  x.addEventListener("click", function () {
+    y.classList.add("show");
     // if (container.className == "cont") {
     //     container.className = "show";
     //   } else {
     //     container.className = "cont";
     //   }
-})
+  });
 
-close.addEventListener('click' , function(){
-    // container.classList.toggle('show');
-    if (container.className == "cont") {
-        container.className = "show";
-      } else {
-        container.className = "cont";
-      }
-})
+  z.addEventListener("click", function () {
+    y.classList.remove('show');
+    // if (y.className == "cont") {
+    //   y.className = "show";
+    // } else {
+    //   y.className = "cont";
+    // }
+  });
+}
+
+modalPopUp(btn , container , clo);
+modalPopUp(btn1 , container1 , clo1);
+modalPopUp(btn2 , container2 , clo2)
+
